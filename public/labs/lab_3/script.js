9deb1d704aad8621e746d26ccc3ef1f9f46318f9
Natalie Selmer <nselmer1@gmail.com>
2021-02-12 21:36:14 -0500

    update
    from jsfiddle test

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,27 @@
 /* Put your javascript in here */
+const listContainer = document.querySelector('.images');
+
+function goLeft() {
+  const last = Array.from(listContainer.children).slice(4, 7).reverse();
+  last.forEach((element) => {
+    listContainer.revmoveChild(element);
+    listContainer.insertBefore(element, listContainer.children[0]);
+  });
+}
+function goRight() {
+  const first = Array.from(listContainer.children).slice(0, 3);
+  first.forEach((element) => {
+    listContainer.revmoveChild(element);
+    listContainer.appendChild(element);
+  });
+}
+
+function loadPage() {
+  document.querySelector('button.arrow.prev').addEventListener('click', (event) => {
+    goLeft();
+  });
+  document.querySelector('button.arrow.next').addEventListener('click', (event) => {
+    goRight();
+  });
+}
+window.onload = loadPage;
