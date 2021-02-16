1183a83c1af0a3f5296b36482bdfb9d469934807
Natalie Selmer <nselmer1@gmail.com>
2021-02-15 22:31:42 -0500

    spacebar for control

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -15,11 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   let timerId = setInterval(startGame,20)
 
+  function control(e){
+      if (e.keyCode == 32)
+      jump()/* 32 is the keycode for the spacebar */
+  }
+
 function jump() {
-    birdBottom += 50
+    /* prevents the bird from jumping off of the top */
+    if ( birdBottom < 500) birdBottom += 50
     bird.style.bottom = birdBottom + 'px'
 }
 
-document.addEventListener('keyup', jump)
+document.addEventListener('keyup', control)
 
 })
