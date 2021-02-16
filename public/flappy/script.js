ef2c3e6d02ddaa1def6cbf41217e89d6680c05da
Natalie Selmer <nselmer1@gmail.com>
2021-02-15 22:20:27 -0500

    bird in potision

---
+++ public/flappy/script.js
@@ -0,0 +1,15 @@
+/* we need to wait for all HTML to load before reading JS. this event listener does that */
+document.addEventListener('DOMContentLoaded', () => {
+  const bird = document.querySelector('.bird')
+  const gameDisplay = document.querySelector('.game-container')
+  const ground = document.querySelector('.ground')
+
+  let birdLeft = 220
+  let birdBottom = 100
+
+  function startGame(){
+      bird.style.bottom = birdBottom + 'px'
+    bird.style.left = birdLeft + 'px'
+  }
+  startGame()
+})
