96426e908894ed236baca579aacaeb5843487740
Natalie Selmer <nselmer1@gmail.com>
2021-02-16 14:51:58 -0500

    updated js

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -1,38 +1,37 @@
-/* we need to wait for all HTML to load before reading JS. this event listener does that */
 document.addEventListener('DOMContentLoaded' , () => {
     const bird = document.querySelector('.bird')
     const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground-moving')
 
-  let birdLeft = 220;
-  let birdBottom = 100;
-  let gravity = 3;
-  let isGameOver = false;
-  let gap = 430;
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 3
+    let isGameOver = false
+    let gap = 430
 
-  function startGame() {
-    birdBottom -= gravity;
-    bird.style.bottom = birdBottom + 'px'
-    bird.style.left = birdLeft + 'px'
-  }
-  let gameTimerId = setInterval(startGame, 20)
 
-  /* 32 is the keycode for the spacebar */
-  function control(e) {
-    if (e.keyCode === 32) {
-      jump()
+    function startGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
     }
-  }
+    let gameTimerId = setInterval(startGame, 20)
 
-  function jump() {
-    /* prevents the bird from jumping off of the top */
-    if (birdBottom < 500) birdBottom += 50
-    bird.style.bottom = birdBottom + 'px'
-    console.log(birdBottom)
-  }
-  document.addEventListener('keyup', control)
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
 
-  function generateObstacle() {
+    function jump() {
+        if (birdBottom < 500) birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+    }
+    document.addEventListener('keyup', control)
+
+
+    function generateObstacle() {
         let obstacleLeft = 500
         let randomHeight = Math.random() * 60
         let obstacleBottom = randomHeight
