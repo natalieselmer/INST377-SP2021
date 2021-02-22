28947119c1ea39e208639b657bc30a070ea27a49
Natalie Selmer <nselmer1@gmail.com>
2021-02-22 14:05:03 -0500

    jumping
    fall and jump functions

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const doodler = docuement.createElement('div')  
   let doodlerLeftSpace = 50
   let doodlerBottomSpace = 150
   let isGameOver = false
   let platformCount = 5
+  let platforms = []
+  let upTimerId 
+  let downTimerId
 
   function createDoodler() {
     grid.appendChild(doodler)
@@ -13,34 +16,66 @@ document.addEventListener('DOMContentLoaded', () =>{
     doodler.style.bottom = doodlerBottomSpace + 'px'
 }
 
-class Platform {
+  class Platform {
     constructor(newPlatBottom) {
-        this.bottom = newPlatBottom
-        this.left = Math.random() * 315
-        this.visual = document.createElement('div')
+      this.left = Math.random() * 315
+      this.bottom = newPlatBottom
+      this.visual = document.createElement('div')
 
-        const visual = this.visual
-        visual.classList.add('platform')
-        visual.style.left = this.left + 'px'
-        visual.style.bottom = this.bottom + 'px'
-        grid.appendChild(visual)
-        
-
+      const visual = this.visual
+      visual.classList.add('platform')
+      visual.style.left = this.left + 'px'
+      visual.style.bottom = this.bottom + 'px'
+      grid.appendChild(visual)
     }
-}
+  }
 
-function createPlatforms(){
-    for (let i = 0; i < platformCount; i++) {
-        let platGap = 600 / platformCount
-        let newPlatBottom = 100 + i * platGap
-        let newPlatform = new Platform(newPlatBottom) 
+  function createPlatforms() {
+    for(let i =0; i < platformCount; i++) {
+      let platGap = 600 / platformCount
+      let newPlatBottom = 100 + i * platGap
+      let newPlatform = new Platform (newPlatBottom)
+      platforms.push(newPlatform)
+      console.log(platforms)
+      
     }
-}
+  }
+
+  function movePlatforms(){
+      if (doodlerBottomSpace > 200){
+          platforms.forEach(platform => {
+              platform.bottom -=4
+              let visual = platform.visual
+              visual.style.bottom = platform.bottom + 'px'
+             })
+      }
+  }
+
+  function jump(){
+      clearInterval(downTimerId)
+      upTimerId = setInterval(function () {
+        doodlerBottomSpace += 20
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+        if (doodlerBottomSpace > 350) {
+            fall()
+        }
+      },30)
+  }
+
+  function fall(){
+      clearInterval(upTimerId)
+      downTimerId = setInterval(function () {
+          doodlerBottomSpace -=5
+          doodler.style.bottom = doodlerBottomSpace + 'px'
+      },30)
+  }
+
 
 function start(){
     if (!isGameOver) {
     createDoodler()
     createPlatforms()
+    setInterval(movePlatforms, 30)
     }
 }
 // attach to button
