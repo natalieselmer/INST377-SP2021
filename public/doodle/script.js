51502aeaf14703c5e635a44c6a779937c3642880
Natalie Selmer <nselmer1@gmail.com>
2021-02-22 14:46:51 -0500

    score

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -2,13 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const doodler = docuement.createElement('div')  
   let doodlerLeftSpace = 50
-  let doodlerBottomSpace = 150
+    let startPoint = 150
+  let doodlerBottomSpace = startPoint
   let isGameOver = false
   let platformCount = 5
   let platforms = []
   let upTimerId 
   let downTimerId
   let isJumping  = false 
+  let isGoingLeft = false
+  let isGoingRight = false
+  let leftTimerId 
+  let rightTimerId
+  let score = 0
+
 
   function createDoodler() {
     grid.appendChild(doodler)
@@ -49,6 +56,16 @@ document.addEventListener('DOMContentLoaded', () => {
               platform.bottom -=4
               let visual = platform.visual
               visual.style.bottom = platform.bottom + 'px'
+              
+              if (platform.bottom < 10) {
+                  let firstPlatform = platforms[0].visual
+                  firstPlatform.classList.remove('platform')
+                  platforms.shift()
+                  score ++
+                  console.log(platforms)
+                  let newPlatform = new Platform(600)
+                  platforms.push(newPlatform)
+              }
              })
       }
   }
@@ -59,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
       upTimerId = setInterval(function () {
         doodlerBottomSpace += 20
         doodler.style.bottom = doodlerBottomSpace + 'px'
-        if (doodlerBottomSpace > 350) {
+        if (doodlerBottomSpace > startPoint + 200) {
             fall()
         }
       },30)
@@ -79,8 +96,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 (doodlerBottomSpace >= platform.bottom) &&
                (doodlerBottomSpace <= platforms.bottom + 15)
                ((doodlerLeftSpace + 60)>= platform.left) &&
-               (doodlerLeftSpace <= + (platform.left + 85))  
-              )
+               (doodlerLeftSpace <= + (platform.left + 85))
+               !isJumping  
+              ){
+                  console.log('landed')
+                  startPoint = doodlerBottomSpace
+                  jump()
+              }
           })
       },30)
   }
@@ -88,26 +110,72 @@ document.addEventListener('DOMContentLoaded', () => {
   function gameOver(){
       console.log('game over')
       isGameOver = true
+      while (grid.firstChild){
+          grid.removeChild(grid.firstChild)
+      }
+      grid.innerHTML = score
       clearInterval(upTimerId)
       clearInterval(downTimerId)
+      clearInterval(leftTimerId)
+      clearInterval(rightTimerId)
+
   }
 
   function control(e){
     if (e.key === 'ArrowLeft'){
-        //moveLeft 
+        moveLeft()
     } else if(e.key === 'ArrowRight'){
-        //moveRight
+        moveRight()
     }else if(e.key ==='ArrowUp'){
-           // moveStraight
+        moveStraight()
         }
     }
   }
+
+  function moveLeft(){
+      if (isGoingRight){
+          clearInterval(rightTimerId)
+          isGoingRight = false
+      }
+      isGoingLeft = true
+      leftTimerId - setInterval(function(){
+          if(doodlerLeftSpace >=0){
+        doodlerLeftSpace -= 5
+        doodler.style.left = doodlerLeftSpace + 'px'
+          }else moveRight()
+
+      }, 20)
+  }
+
+ function moveRight(){
+           if (isGoingLeft){
+          clearInterval(leftTimerId)
+          isGoingLeft = false
+           }
+      isGoingRight = true
+      rightTimerId - setInterval(function(){
+          if(doodlerLeftSpace <=340){
+        doodlerLeftSpace += 5
+        doodler.style.left = doodlerLeftSpace + 'px'
+          }else moveLeft()
+
+      }, 20)
+    }
+
+    function moveStraight(){
+        isGoingRight = false
+        isGoingLeft = false
+        clearInterval(rightTimerId)
+        clearInterval(leftTimerId)
+    }
 
 function start(){
     if (!isGameOver) {
     createPlatforms()
     createDoodler()
     setInterval(movePlatforms, 30)
+    jump()
+    document.addEventListener('keyup', control)
     }
 }
 // attach to button
