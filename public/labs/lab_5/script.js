288dba70f25c8d67aadf5175cc2f0e48053a3f4a
Natalie Selmer <nselmer1@gmail.com>
2021-03-09 12:52:34 -0500

    map div id
    created const in js of mymap

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,44 +1,39 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
+  const mymap = L.map('mapid').setView([-76.9378, 38.9897], 13)
+  
   return map;
 }
 
 async function dataHandler(mapObjectFromFunction) {
   // use your assignment 1 data handling code here
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
 
-/*let places = [];
-fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
-.then(function(blob) {
-    return blob.json();
-})
-.then(function(data) {
- places = data;
-})*/
+  let places = [];
+  async function windowActions() {
+    console.log('window loaded');
+    const request = await fetch(
+      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
+    );
+    places = await request.json([]);
+  }
 
-let places = []
-async function windowActions() {
-   console.log('window loaded');
-const request = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-places = await request.json([]);
-};
-
-
-function displayMatches(e){
+  function displayMatches(e) {
     e.preventDefault();
 
-    const matchArray = places.filter(function(place){
-        
-        return place.city.includes(searchInput.value) || place.zip.includes(searchInput.value);
+    const matchArray = places.filter(function(place) {
+      return (
+        place.city.includes(searchInput.value) ||
+        place.zip.includes(searchInput.value)
+      );
     });
-
 
     console.log(matchArray);
 
-const HTML = matchArray.reduce((string, place, index) => {
-        string += `
+    const HTML = matchArray.reduce((string, place, index) => {
+      string += `
             <li class="box">
                 <div class='name'>${place.name.toUpperCase()}</div>
                 <div class='category'>${place.category.toUpperCase()}</div>
@@ -47,14 +42,15 @@ const HTML = matchArray.reduce((string, place, index) => {
                 <div class='zip'>${place.zip}</div>
 
             </li>
-        `
-    return string}, '');
-   
+        `;
+      return string;
+    }, "");
+
     suggestions.innerHTML = HTML;
-console.log(HTML)
-}
-document.querySelector("form").addEventListener('submit', displayMatches);
-window.onload=windowActions();
+    console.log(HTML);
+  }
+  document.querySelector('form').addEventListener('submit', displayMatches);
+  window.onload = windowActions();
   // and target mapObjectFromFunction to attach markers
 }
 
