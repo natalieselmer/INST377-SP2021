59bdea65c3d089216203262e25ec3f8bc4cf7cf5
Natalie Selmer <nselmer1@gmail.com>
2021-03-09 12:58:35 -0500

    submit button and map added

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,7 +1,15 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
   const mymap = L.map('mapid').setView([-76.9378, 38.9897], 13)
-  
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
   return map;
 }
 
